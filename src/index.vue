b1da6303e18daf4d21872073fcebfa35b17b9d73
dimpurr <dimpurr@live.com>
2018-05-03 11:55:00 +0800

    page most fin

--- src/index.vue
+++ src/index.vue
@@ -34,6 +34,109 @@
         <p>喜欢绘画可惜是个手残，想学编曲但是五音不全，不知何时能成为一位能自己画 PV 的 Vocaloid Producer 。偶尔写点故事。</p>
     </div>
 </div>
+<div class="psection circle ctn">
+    <h3 class="h">圈子</h3>
+    <ul class="clist">
+        <li><a href="#">
+            <h5>我的圈子<small class="count">68</small></h5>
+            <ul class="avatar_group">
+                <li class="more"> ... </li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+            </ul>
+        </a></li>
+        <li><a href="#">
+            <h5>主页</h5>
+            <ul class="avatar_group">
+                <li class="more"> ... </li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+            </ul>
+        </a></li>
+        <li><a href="#">
+            <h5>主页</h5>
+            <ul class="avatar_group">
+                <li class="more"> ... </li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+                <li><a href="#"><img class="head_circle" src="/static/head.jpg"></a></li>
+            </ul>
+        </a></li>
+    </ul>
+</div>
+<div class="psection link ctn">
+    <h3 class="h">链接</h3>
+    <ul class="clist">
+        <li><a href="#">
+            <h5>主页</h5>
+            <p>dimpurr.com</p>
+        </a></li>
+        <li><a href="#">
+            <h5>主页</h5>
+            <p>dimpurr.com</p>
+        </a></li>
+        <li><a href="#">
+            <h5>主页</h5>
+            <p>dimpurr.com</p>
+        </a></li>
+    </ul>
+</div>
+<div class="psection idea skill ctn">
+    <h3 class="h">技能点和认可数</h3>
+    <section class="tlist_group">
+        <h4>项目意愿</h4>
+        <ul class="tlist">
+            <li><a href="#">愿意加入项目</a></li>
+            <li><a href="#">交互设计<small class="count">12</small></a></li>
+            <li><a href="#">交互设计<small class="count">12</small></a></li>
+        </ul>
+    </section>
+    <section class="tlist_group">
+        <h4>项目意愿</h4>
+        <ul class="tlist">
+            <li><a href="#">愿意加入项目</a></li>
+            <li><a href="#">交互设计<small class="count">12</small></a></li>
+            <li><a href="#">交互设计<small class="count">12</small></a></li>
+        </ul>
+    </section>
+    <section class="tlist_group">
+        <h4>项目意愿</h4>
+        <ul class="tlist">
+            <li><a href="#">愿意加入项目</a></li>
+            <li><a href="#">交互设计<small class="count">12</small></a></li>
+            <li><a href="#">交互设计<small class="count">12</small></a></li>
+        </ul>
+    </section>
+</div>
+<div class="psection idea myidea ctn">
+    <h3 class="h">发起的 IDEA</h3>
+    <ul class="clist clist_width">
+        <li><a href="#">
+            <h5>
+                <span class="conf">第三届互联网+创业创新大赛</span>
+                <span class="ideaname">BYRCAFE 信息聚合平台</span>
+                <small>以链接形式聚合 BYRBSS 、各学生社团微信公众号和其他社区创作内容的 ...</small></h5>
+            <p>
+                <span class="member">已有<ul><li>产品设计</li><li>React前端开发<small class="count">3</small></li></ul></span>
+                <span class="need">招募<ul><li>Node.js后端开发</li></ul></span>
+            </p>
+        </a></li>
+    </ul>
+</div>
 </div>
 </template>
 
@@ -50,13 +153,65 @@ module.exports = {
 <style scoped lang="stylus">
 topnav_height = 48px
 banner_height = 400px
+nolist-ul()
+    padding 0
+    margin 0
+    list-style none
+.tlist_group
+    display inline-block
+    margin-bottom 16px
+    h4
+        font-size 14px
+        font-weight normal
+        color #777
+        margin 4px 0
+.tlist
+    nolist-ul()
+    li
+        display inline-block
+        a
+            text-decoration none
+            color #4c4c4c
+            font-size 24px
+            margin-right 16px
+            vertical-align bottom
+            line-height 26px
+            .count
+                font-size 26px
+                color #CCC
+                margin-left 5px
+.count
+    &::before
+        content "+"
+.avatar_group
+    nolist-ul()
+    > li
+        display inline-block
+        float right
+        margin-right -12px
+        a img, &.more
+                width 32px
+                border 2px solid #FFFFFFAA
+                border-radius 100%
+                // &:first-of-type
+        &.more
+            margin-right 0
+            width 32px
+            height 32px
+            background #dfdfdf
+            text-align center
+            color #FFF
+            font-size 24px
+            line-height 20px
+    &::after
+        content ""
+        display block
+        clear both
 .topnav
     box-sizing border-box
     height topnav_height
     ul
-        list-style none
-        margin 0
-        padding 0
+        nolist-ul()
         li
             display inline
             vertical-align middle
@@ -136,4 +291,68 @@ banner_height = 400px
         line-height 1.75em
         p
             margin 0.8em 0
+    .clist
+        nolist-ul()
+        & > li
+            display inline-block
+            margin-right 4px
+            margin-bottom 8px
+            vertical-align top
+            & > a
+                display block
+                text-decoration none
+                padding 19px 26px
+                background #e0e0e0
+                h5
+                    color #000
+                    margin 1px 0 3px 0
+                    .count
+                        color #7a7a7a
+                        font-size 14px
+                        margin-left 6px
+                p
+                    color #707070
+                    margin 0
+                .avatar_group
+                    margin-top 8px
+                    margin-bottom -2px
+        &.clist_width > li
+            display block
+            margin 8px 0
+            & > a
+                padding 16px 26px
+    &.myidea
+        .clist_width > li > a
+            h5
+                height 15px
+                white-space:nowrap;
+                overflow hidden
+                text-overflow ellipsis
+                color #5b5b5b
+                .conf
+                    color #23a0f2
+                    &::after
+                        content " · "
+                        color #5b5b5b
+                .ideaname
+                    color #000
+                small 
+                    color #5b5b5b
+                    font-size 13px
+                    font-weight normal
+                    &::before
+                        content " · "
+            p
+                font-size 13px
+                margin-top 8px
+                .member, .need
+                    display inline
+                ul
+                    nolist-ul()
+                    display inline
+                    margin-left 12px
+                    li
+                        display inline
+                        margin-right 12px
+                        color #070092
 </style>
